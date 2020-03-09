0d2e736eb6929ccc32e18c10d003adb811dc9b4a
Yong Jia Jie <yjjyongjiajie@gmail.com>
2020-03-09 20:46:33 +0800

    Replaced @RequestMapping with @GetMapping in UserController.java

--- src/main/java/sg/com/example/demo/controller/UserController.java
+++ src/main/java/sg/com/example/demo/controller/UserController.java
@@ -1,7 +1,7 @@
 package sg.com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sg.com.example.demo.model.User;
 import sg.com.example.demo.model.UserRepository;
@@ -15,7 +15,7 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
-    @RequestMapping("/users")
+    @GetMapping("/users")
     public UserResults findUsers() {
         List<User> users = (List<User>) userRepository.findBySalaryBetween(0.00, 4000.00);
         return new UserResults(users);
